1142544c8d056b60fb8b4e35a5f61bc334a227d3
wiyadathi <wiyada.t@bu.ac.th>
2025-03-08 18:05:50 +0700

    create new scene add new objects and Gravity work

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 public class Gravity : MonoBehaviour
 {
     Rigidbody rb;
-    const float G = 667.4f; //Gravitational Constant 6.674
+    const float G = 0.006674f; //Gravitational Constant 6.674
 
     //create a List of objects in the galaxy to attract
-    public static List<Gravity> GravityObjectsList;
+    public static List<Gravity> otherObjectsList;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
 
         //create a list for the first time
-        if (GravityObjectsList == null)
+        if (otherObjectsList == null)
         {
-            GravityObjectsList = new List<Gravity>();
+            otherObjectsList = new List<Gravity>();
         }
 
         //add object (with gravity script) to attract to the list
-        GravityObjectsList.Add(this);
+        otherObjectsList.Add(this);
 
     }
 
     private void FixedUpdate()
     {
-        foreach (Gravity obj in GravityObjectsList) 
+        foreach (Gravity obj in otherObjectsList) 
         {
             //do not attract itself
             if (obj != this)
@@ -52,7 +52,7 @@ public class Gravity : MonoBehaviour
 
         //calculate gravitational force between objs
         //F = G*((m1*m2)/r*r)
-        float forceMagnitude = G * ((rb.mass * otherRb.mass)/Mathf.Pow(distance, 2));
+        float forceMagnitude = G * (rb.mass * otherRb.mass)/Mathf.Pow(distance, 2);
 
         //Combine force magnitude with its direction (normalize)
         //to form final gravitaional force (Vector3)
