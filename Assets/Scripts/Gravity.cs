8a7cfc4ff78081c3198dfd4da7195ee129c13dad
wiyadathi <wiyada.t@bu.ac.th>
2025-03-08 14:55:49 +0700

    Update Gravity.cs

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,16 +1,67 @@
+using NUnit.Framework;
+using System.Collections.Generic; //for List
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+    const float G = 667.4f; //Gravitational Constant 6.674
+
+    //create a List of objects in the galaxy to attract
+    public static List<Gravity> GravityObjectsList;
+;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        //create a list for the first time
+        if (GravityObjectsList == null)
+        {
+            GravityObjectsList = new List<Gravity>();
+        }
+
+        //add object (with gravity script) to attract to the list
+        GravityObjectsList.Add(this);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        foreach (Gravity obj in GravityObjectsList) 
+        {
+            //do not attract itself
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
+
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+
+        //get direction between 2 objects
+        Vector3 direction = rb.position - otherRb.position;
+
+        //get only distance between 2 obj
+        float distance = direction.magnitude;
+
+        //if 2 obj are at the same position,just return=do nothing to avoid collision
+        if (distance == 0f) { return; }
+
+        //calculate gravitational force between objs
+        //F = G*((m1*m2)/r*r)
+        float forceMagnitude = G * ((rb.mass * otherRb.mass)/Mathf.Pow(distance, 2));
+
+        //Combine force magnitude with its direction (normalize)
+        //to form final gravitaional force (Vector3)
+        Vector3 gravityForce = forceMagnitude * direction.normalized;
+
+        //apply gravitation force to other obj
+        otherRb.AddForce(gravityForce);
+
+        //finally call this method in FixedUpdate
     }
 }
